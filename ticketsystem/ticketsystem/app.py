fd9a3a2fc851d2f276456bce9d702d1f3fad7a38
Fluppenmann <fluppensammler@gmail.com>
2026-03-13 10:18:42 +0100

    Add files via upload

---
+++ ticketsystem/ticketsystem/app.py
@@ -0,0 +1,107 @@
+from flask import Flask, render_template, request, redirect, session
+from users import users
+
+app = Flask(__name__)
+app.secret_key = "secretkey"
+
+tickets = [
+    {"id":1,"name":"Login funktioniert nicht","creator":"Max","agent":"Support-Team","status":"Offen"},
+    {"id":2,"name":"Laptop defekt","creator":"Arda","agent":"Support-Team","status":"In Bearbeitung"},
+    {"id":3,"name":"Ticket schließen","creator":"Arda","agent":"Max","status":"Geschlossen"}
+]
+
+
+@app.route("/")
+def index():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    username = request.form["username"]
+    password = request.form["password"]
+
+    if username in users and users[username]["password"] == password:
+
+        session["user"] = username
+        session["role"] = users[username]["role"]
+
+        return redirect("/meine_tickets")
+
+    return redirect("/")
+
+
+@app.route("/meine_tickets")
+def meine_tickets():
+
+    if "user" not in session:
+        return redirect("/")
+
+    user = session["user"]
+
+    user_tickets = [t for t in tickets if t["creator"].lower() == user.lower()]
+
+    return render_template(
+        "meine_tickets.html",
+        tickets=user_tickets,
+        role=session["role"]
+    )
+
+
+@app.route("/ticketpool")
+def ticketpool():
+
+    if "user" not in session:
+        return redirect("/")
+
+    return render_template(
+        "ticketpool.html",
+        tickets=tickets,
+        role=session["role"]
+    )
+
+
+@app.route("/auswertung")
+def auswertung():
+
+    if "user" not in session:
+        return redirect("/")
+
+    return render_template(
+        "auswertung.html",
+        role=session["role"]
+    )
+
+
+@app.route("/login")
+def logout():
+
+    session.clear()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+@app.route("/create_ticket", methods=["POST"])
+def create_ticket():
+
+    if "user" not in session:
+        return redirect("/")
+
+    name = request.form["name"]
+    agent = request.form["agent"]
+    status = request.form["status"]
+
+    new_ticket = {
+        "id": len(tickets) + 1,
+        "name": name,
+        "creator": session["user"],
+        "agent": agent,
+        "status": status
+    }
+
+    tickets.append(new_ticket)
+
+    return redirect("/meine_tickets")
---
+++ ticketsystem/ticketsystem/users.py
@@ -0,0 +1,14 @@
+users = {
+    "admin": {
+        "password": "admin123",
+        "role": "admin"
+    },
+    "max": {
+        "password": "1234",
+        "role": "support"
+    },
+    "arda": {
+        "password": "1234",
+        "role": "user"
+    }
+}
